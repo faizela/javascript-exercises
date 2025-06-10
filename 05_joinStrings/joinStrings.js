880bc055f2b782a15c20292e2bdb8f1357ff1088
Faisal Ahmed <feisala132@gmail.com>
2025-06-10 10:48:06 +0100

    solved joinstrex

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -9,6 +9,15 @@
 
 // Add your code right below, good luck!
 
+let firstName = 'Carlos'
+let lastName = 'Stevenson'
+let birthYear = 1947
+let thisYear = 1965
+let age = thisYear - birthYear
+let fullName = firstName + ' ' + lastName
+
+let greeting = `Hello! My name is ${fullName} and I am ${age} years old.`
+
 
 
 
